ee6293322919837299ddb3a9aaff71c315ffd140
imortensen <38968943+imortensen@users.noreply.github.com>
2020-11-17 08:27:16 -0700

    server

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const path = require('path')
 const app = express()
 
 // Serve only the static files form the dist directory
+app.use(express.static('./dist/restaurant-rater-client'))
 // app.use(express.static(__dirname + '/dist/restaurant-rater-client'))
-app.use(express.static('restaurant-rater-client/dist/restaurant-rater-client'))
 
 app.get('/*', function (req, res) {
-  res.sendFile(
-    // path.join(__dirname + '/dist/restaurant-rater-client/index.html')
-    path.join('restaurant-rater-client/dist/restaurant-rater-client/index.html')
-  )
+  // res.sendFile(
+  //   path.join(__dirname + '/dist/restaurant-rater-client/index.html')
+  // )
+  res.sendFile('index.html', { root: 'dist/restaurant-rater-client/' })
 })
 
 // Start the app by listening on the default Heroku port
